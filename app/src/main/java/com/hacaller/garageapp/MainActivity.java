5fe782c5897bb671816f6b82f388043f4b686b15
herb786 <herbertacg@gmail.com>
2018-04-09 15:43:59 -0500

    Insert activities.

--- app/src/main/java/com/hacaller/garageapp/MainActivity.java
+++ app/src/main/java/com/hacaller/garageapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hacaller.garageapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnOffers = findViewById(R.id.offers);
+        Button btnFaqs = findViewById(R.id.faq);
+
+
+        btnOffers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CarServiceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnFaqs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FAQActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
